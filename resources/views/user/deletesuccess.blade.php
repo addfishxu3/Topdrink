38b14d5dc640c710fbf050ca99d66914612daea4
addfishxu3 <108409983+addfishxu3@users.noreply.github.com>
2024-05-09 22:41:49 +0800

    Update deletesuccess.blade.php

--- resources/views/user/deletesuccess.blade.php
+++ resources/views/user/deletesuccess.blade.php
@@ -1,5 +1,5 @@
 <?php
 
- echo ("<script>alert('註銷成功！');location='http://127.0.0.1:8000/customer'</script>");
+ echo ("<script>alert('註銷成功！');location='http://127.0.0.1:8000/customer';</script>");
 
 ?>
